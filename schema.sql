084dcfb8cf6000d2885a51bd377dc84566adc617
Carlos <cdzao@yahoo.com>
2026-08-29 09:05:04 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,9 @@
+CREATE TABLE IF NOT EXISTS logs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    workspace_id TEXT NOT NULL,
+    date TEXT NOT NULL,
+    topic TEXT NOT NULL,
+    notes TEXT NOT NULL
+);
 
+CREATE INDEX IF NOT EXISTS idx_workspace_date ON logs (workspace_id, date);
